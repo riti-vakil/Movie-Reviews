a9bfbf2b431c1d515a910bd856aa6018e6f3fb33
unknown <ritivakil3@gmail.com>
2023-11-29 18:13:15 +0530

    Final Version

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import queryString from "query-string";
 
-// const baseURL = "https://moonflix-api.vercel.app/api/v1/";
 const baseURL = "http://localhost:3000/api/v1/";
 const privateClient = axios.create({
   baseURL,
